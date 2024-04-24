c7e114bc8128a8b365da9b65ac9c62810f3a4216
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-24 17:53:23 -0300

    feat: automatic input to search for whatever I want, for example "Airplane video"

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get('https://google.com')
+
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'gLFyf')))
+
+input_element = driver.find_element(By.CLASS_NAME, 'gLFyf')
+input_element.clear()
+input_element.send_keys("Avião video" + Keys.ENTER)
